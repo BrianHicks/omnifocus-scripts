a7b040d87812717ce2a7e5fd09d97759fad43e54
Brian Hicks <brian@brianthicks.com>
2022-10-13 22:41:57 -0500

    clean up strategies list

--- Gardening.omnifocusjs/Resources/pick.ts
+++ Gardening.omnifocusjs/Resources/pick.ts
@@ -171,15 +171,10 @@
 
   var action = new PlugIn.Action(async () => {
     try {
-      // TODO: chosoe a strategy randomly from:
+      // TODO: more strategies:
       //
-      // - process inbox tasks (if any). Weight is number of inbox tasks.
-      // - review project (with prompt like "how will this make the world better
-      //   when it's done?" or "are you going to make any progress on this during
-      //   the next week?"). Weight is number of unreviewed projects.
       // - reflection prompts (constant weight)
       // - stuff stolen from Taylor's nowify prompts
-      // - the "choose a task" strategy below
       let strategies = [
         new ProcessInbox(),
         new ReviewProjects(),
