c40f3eea223790aa6579b9ab836b5ce88c4f154b
Brian Hicks <brian@brianthicks.com>
2022-10-13 22:03:03 -0500

    consolidate into strategies

--- Gardening.omnifocusjs/Resources/pick.js
+++ Gardening.omnifocusjs/Resources/pick.js
@@ -1,37 +1,6 @@
 "use strict";
 (() => {
-    var getWeights = function () {
-        const now = new Date();
-        const hour = now.getHours();
-        const day = now.getDay();
-        if (hour >= 8 && hour <= 17 && day != 0 && day != 6) {
-            return { work: 0.8, personal: 0.2 };
-        }
-        else {
-            return { work: 0.0, personal: 1.0 };
-        }
-    };
-    var categorizeTask = function (task) {
-        var todo = task.tags;
-        while (todo.length != 0) {
-            let tag = todo.pop();
-            if (tag.name == "work") {
-                return "work";
-            }
-            else if (tag.name == "personal") {
-                return "personal";
-            }
-            else if (tag.parent) {
-                todo.push(tag.parent);
-            }
-        }
-        return null;
-    };
-    var daysBetween = function (a, b) {
-        let millis = Math.abs(a.getTime() - b.getTime());
-        return millis / 1000 / 60 / 60 / 24;
-    };
-    var weightedRandom = function (pairs) {
+    function weightedRandom(pairs) {
         var total = 0;
         for (let pair of pairs) {
             total += pair[1];
@@ -44,54 +13,105 @@
             }
         }
         return null;
-    };
-    var chooseATask = function () {
-        let weights = getWeights();
-        let now = new Date();
-        let possibleTasks = flattenedProjects
-            .filter((p) => p.status == Project.Status.Active)
-            .flatMap((p) => p.flattenedTasks.filter((t) => t.taskStatus == Task.Status.Available ||
-            t.taskStatus == Task.Status.DueSoon ||
-            t.taskStatus == Task.Status.Next ||
-            t.taskStatus == Task.Status.Overdue));
-        let weightedTasks = [];
-        for (let task of possibleTasks) {
-            // weight some categories higher than others
-            let categoryWeight = 0;
-            let category = categorizeTask(task);
-            if (category) {
-                categoryWeight = weights[category];
-            }
-            // weight stale-er tasks higher, up to 7 days
-            let ageWeight = Math.min(7, daysBetween(now, task.modified)) / 7;
-            // weight due-er tasks higher, up to 100 points
-            let dueWeight = 0;
-            if (task.effectiveDueDate) {
-                dueWeight = 100 - daysBetween(now, task.effectiveDueDate);
-            }
-            weightedTasks.push([task, ageWeight + dueWeight + categoryWeight]);
+    }
+    class ChooseATask {
+        constructor() {
+            this.tasks = flattenedProjects
+                .filter((p) => p.status == Project.Status.Active)
+                .flatMap((p) => p.flattenedTasks.filter((t) => t.taskStatus == Task.Status.Available ||
+                t.taskStatus == Task.Status.DueSoon ||
+                t.taskStatus == Task.Status.Next ||
+                t.taskStatus == Task.Status.Overdue));
         }
-        let chosenTask = weightedRandom(weightedTasks);
-        if (chosenTask) {
-            document.windows[0].perspective = Perspective.BuiltIn.Projects;
-            if (chosenTask.containingProject) {
-                document.windows[0].focus = [
-                    chosenTask.containingProject,
-                ];
-            }
-            document.windows[0].selectObjects([chosenTask]);
+        weight() {
+            return this.tasks.length;
         }
-    };
+        enact() {
+            let weights = this.getWeights();
+            let now = new Date();
+            let weightedTasks = [];
+            for (let task of this.tasks) {
+                // weight some categories higher than others
+                let categoryWeight = 0;
+                let category = this.categorizeTask(task);
+                if (category) {
+                    categoryWeight = weights[category];
+                }
+                // weight stale-er tasks higher, up to 7 days
+                let ageWeight = 0;
+                if (task.modified) {
+                    ageWeight = Math.min(7, this.daysBetween(now, task.modified)) / 7;
+                }
+                // weight due-er tasks higher, up to 100 points
+                let dueWeight = 0;
+                if (task.effectiveDueDate) {
+                    dueWeight = 100 - this.daysBetween(now, task.effectiveDueDate);
+                }
+                weightedTasks.push([task, ageWeight + dueWeight + categoryWeight]);
+            }
+            let chosenTask = weightedRandom(weightedTasks);
+            if (chosenTask) {
+                document.windows[0].perspective = Perspective.BuiltIn.Projects;
+                if (chosenTask.containingProject) {
+                    document.windows[0].focus = [
+                        chosenTask.containingProject,
+                    ];
+                }
+                document.windows[0].selectObjects([chosenTask]);
+            }
+        }
+        getWeights() {
+            const now = new Date();
+            const hour = now.getHours();
+            const day = now.getDay();
+            if (hour >= 8 && hour <= 17 && day != 0 && day != 6) {
+                return { work: 0.8, personal: 0.2 };
+            }
+            else {
+                return { work: 0.0, personal: 1.0 };
+            }
+        }
+        categorizeTask(task) {
+            var todo = task.tags;
+            while (todo.length != 0) {
+                let tag = todo.pop();
+                if (tag.name == "work") {
+                    return "work";
+                }
+                else if (tag.name == "personal") {
+                    return "personal";
+                }
+                else if (tag.parent) {
+                    todo.push(tag.parent);
+                }
+            }
+            return null;
+        }
+        daysBetween(a, b) {
+            let millis = Math.abs(a.getTime() - b.getTime());
+            return millis / 1000 / 60 / 60 / 24;
+        }
+    }
     var action = new PlugIn.Action(async () => {
         try {
             // TODO: chosoe a strategy randomly from:
             //
             // - process inbox tasks (if any). Weight is number of inbox tasks.
-            // - review project (with prompt like "how will this make the world better when it's done?" or "are you going to make any progress on this during the next week?"). Weight is number of unreviewed projects.
+            // - review project (with prompt like "how will this make the world better
+            //   when it's done?" or "are you going to make any progress on this during
+            //   the next week?"). Weight is number of unreviewed projects.
             // - reflection prompts (constant weight)
             // - stuff stolen from Taylor's nowify prompts
             // - the "choose a task" strategy below
-            chooseATask();
+            let strategies = [new ChooseATask()];
+            let weightedStrategies = strategies.map((s) => [
+                s,
+                s.weight(),
+            ]);
+            let chosen = weightedRandom(weightedStrategies);
+            if (chosen) {
+                chosen.enact();
+            }
         }
         catch (err) {
             console.error(err);
--- Gardening.omnifocusjs/Resources/pick.ts
+++ Gardening.omnifocusjs/Resources/pick.ts
@@ -1,39 +1,10 @@
 (() => {
-  var getWeights = function (): { work: number; personal: number } {
-    const now = new Date();
-    const hour = now.getHours();
-    const day = now.getDay();
+  interface Strategy {
+    weight(): number;
+    enact(): void;
+  }
 
-    if (hour >= 8 && hour <= 17 && day != 0 && day != 6) {
-      return { work: 0.8, personal: 0.2 };
-    } else {
-      return { work: 0.0, personal: 1.0 };
-    }
-  };
-
-  var categorizeTask = function (task: Task): "work" | "personal" | null {
-    var todo = task.tags;
-    while (todo.length != 0) {
-      let tag = todo.pop();
-
-      if (tag.name == "work") {
-        return "work";
-      } else if (tag.name == "personal") {
-        return "personal";
-      } else if (tag.parent) {
-        todo.push(tag.parent);
-      }
-    }
-
-    return null;
-  };
-
-  var daysBetween = function (a: Date, b: Date): number {
-    let millis = Math.abs(a.getTime() - b.getTime());
-    return millis / 1000 / 60 / 60 / 24;
-  };
-
-  var weightedRandom = function <Type>(pairs: [Type, number][]): Type | null {
+  function weightedRandom<Type>(pairs: [Type, number][]): Type | null {
     var total = 0;
     for (let pair of pairs) {
       total += pair[1];
@@ -49,68 +20,125 @@
     }
 
     return null;
-  };
+  }
 
-  var chooseATask = function () {
-    let weights = getWeights();
-    let now = new Date();
+  class ChooseATask implements Strategy {
+    tasks: Task[];
 
-    let possibleTasks = flattenedProjects
-      .filter((p) => p.status == Project.Status.Active)
-      .flatMap((p) =>
-        p.flattenedTasks.filter(
-          (t: Task) =>
-            t.taskStatus == Task.Status.Available ||
-            t.taskStatus == Task.Status.DueSoon ||
-            t.taskStatus == Task.Status.Next ||
-            t.taskStatus == Task.Status.Overdue
-        )
-      );
-
-    let weightedTasks: [Task, number][] = [];
-
-    for (let task of possibleTasks) {
-      // weight some categories higher than others
-      let categoryWeight = 0;
-      let category = categorizeTask(task);
-      if (category) {
-        categoryWeight = weights[category];
-      }
-
-      // weight stale-er tasks higher, up to 7 days
-      let ageWeight = Math.min(7, daysBetween(now, task.modified)) / 7;
-
-      // weight due-er tasks higher, up to 100 points
-      let dueWeight = 0;
-      if (task.effectiveDueDate) {
-        dueWeight = 100 - daysBetween(now, task.effectiveDueDate);
-      }
-
-      weightedTasks.push([task, ageWeight + dueWeight + categoryWeight]);
+    constructor() {
+      this.tasks = flattenedProjects
+        .filter((p) => p.status == Project.Status.Active)
+        .flatMap((p) =>
+          p.flattenedTasks.filter(
+            (t: Task) =>
+              t.taskStatus == Task.Status.Available ||
+              t.taskStatus == Task.Status.DueSoon ||
+              t.taskStatus == Task.Status.Next ||
+              t.taskStatus == Task.Status.Overdue
+          )
+        );
     }
 
-    let chosenTask = weightedRandom(weightedTasks);
-    if (chosenTask) {
-      document.windows[0].perspective = Perspective.BuiltIn.Projects;
-      if (chosenTask.containingProject) {
-        document.windows[0].focus = [
-          chosenTask.containingProject,
-        ] as SectionArray;
-      }
-      document.windows[0].selectObjects([chosenTask]);
+    weight(): number {
+      return this.tasks.length;
     }
-  };
+
+    enact() {
+      let weights = this.getWeights();
+      let now = new Date();
+
+      let weightedTasks: [Task, number][] = [];
+
+      for (let task of this.tasks) {
+        // weight some categories higher than others
+        let categoryWeight = 0;
+        let category = this.categorizeTask(task);
+        if (category) {
+          categoryWeight = weights[category];
+        }
+
+        // weight stale-er tasks higher, up to 7 days
+        let ageWeight = 0;
+        if (task.modified) {
+          ageWeight = Math.min(7, this.daysBetween(now, task.modified)) / 7;
+        }
+
+        // weight due-er tasks higher, up to 100 points
+        let dueWeight = 0;
+        if (task.effectiveDueDate) {
+          dueWeight = 100 - this.daysBetween(now, task.effectiveDueDate);
+        }
+
+        weightedTasks.push([task, ageWeight + dueWeight + categoryWeight]);
+      }
+
+      let chosenTask = weightedRandom(weightedTasks);
+      if (chosenTask) {
+        document.windows[0].perspective = Perspective.BuiltIn.Projects;
+        if (chosenTask.containingProject) {
+          document.windows[0].focus = [
+            chosenTask.containingProject,
+          ] as SectionArray;
+        }
+        document.windows[0].selectObjects([chosenTask]);
+      }
+    }
+
+    getWeights(): { work: number; personal: number } {
+      const now = new Date();
+      const hour = now.getHours();
+      const day = now.getDay();
+
+      if (hour >= 8 && hour <= 17 && day != 0 && day != 6) {
+        return { work: 0.8, personal: 0.2 };
+      } else {
+        return { work: 0.0, personal: 1.0 };
+      }
+    }
+
+    categorizeTask(task: Task): "work" | "personal" | null {
+      var todo = task.tags;
+      while (todo.length != 0) {
+        let tag = todo.pop();
+
+        if (tag.name == "work") {
+          return "work";
+        } else if (tag.name == "personal") {
+          return "personal";
+        } else if (tag.parent) {
+          todo.push(tag.parent);
+        }
+      }
+
+      return null;
+    }
+
+    daysBetween(a: Date, b: Date): number {
+      let millis = Math.abs(a.getTime() - b.getTime());
+      return millis / 1000 / 60 / 60 / 24;
+    }
+  }
 
   var action = new PlugIn.Action(async () => {
     try {
       // TODO: chosoe a strategy randomly from:
       //
       // - process inbox tasks (if any). Weight is number of inbox tasks.
-      // - review project (with prompt like "how will this make the world better when it's done?" or "are you going to make any progress on this during the next week?"). Weight is number of unreviewed projects.
+      // - review project (with prompt like "how will this make the world better
+      //   when it's done?" or "are you going to make any progress on this during
+      //   the next week?"). Weight is number of unreviewed projects.
       // - reflection prompts (constant weight)
       // - stuff stolen from Taylor's nowify prompts
       // - the "choose a task" strategy below
-      chooseATask();
+      let strategies = [new ChooseATask()];
+      let weightedStrategies: [Strategy, number][] = strategies.map((s) => [
+        s,
+        s.weight(),
+      ]);
+      let chosen = weightedRandom(weightedStrategies);
+      if (chosen) {
+        chosen.enact();
+      }
     } catch (err) {
       console.error(err);
       throw err;
