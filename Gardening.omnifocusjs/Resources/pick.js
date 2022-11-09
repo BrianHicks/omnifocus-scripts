c863f554bf5674dd3a1dfef1bac5063ad27aea91
Brian Hicks <brian@brianthicks.com>
2022-11-09 16:32:35 -0600

    only need one Linear task at once, actually!

--- Gardening.omnifocusjs/Resources/pick.js
+++ Gardening.omnifocusjs/Resources/pick.js
@@ -260,7 +260,7 @@
                 new CheckEmail(),
                 new ReviewProjects(),
                 new FillEmptyProject(),
-                new PullForTag("from Linear", 2),
+                new PullForTag("from Linear", 1),
                 new PullForTag("from GitHub", 1),
             ];
             let weightedStrategies = strategies.map((s) => [
