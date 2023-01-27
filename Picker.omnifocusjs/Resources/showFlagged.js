b7fa0f9740556fa575e34a8707a1d9e9cc11dea6
Brian Hicks <brian@brianthicks.com>
2023-01-27 08:19:44 -0600

    make a view that shows projects with flagged tasks

---
+++ Picker.omnifocusjs/Resources/showFlagged.js
@@ -0,0 +1,27 @@
+"use strict";
+(() => {
+    var action = new PlugIn.Action(async () => {
+        try {
+            let toFocus = [];
+            flattenedProjects
+                .filter((p) => p.status == Project.Status.Active)
+                .flatMap((p) => p.flattenedTasks.filter((t) => t.flagged &&
+                (t.taskStatus == Task.Status.Available ||
+                    t.taskStatus == Task.Status.DueSoon ||
+                    t.taskStatus == Task.Status.Next ||
+                    t.taskStatus == Task.Status.Overdue)))
+                .forEach((task) => {
+                if (task.containingProject && toFocus.indexOf(task.containingProject) === -1) {
+                    toFocus.push(task.containingProject);
+                }
+            });
+            document.windows[0].perspective = Perspective.BuiltIn.Projects;
+            document.windows[0].focus = toFocus;
+        }
+        catch (err) {
+            console.error(err);
+            throw err;
+        }
+    });
+    return action;
+})();
---
+++ Picker.omnifocusjs/Resources/showFlagged.ts
@@ -0,0 +1,33 @@
+(() => {
+  var action = new PlugIn.Action(async () => {
+    try {
+      let toFocus: Project[] = [];
+
+      flattenedProjects
+        .filter((p) => p.status == Project.Status.Active)
+        .flatMap((p) =>
+          p.flattenedTasks.filter(
+            (t: Task) =>
+              t.flagged &&
+              (t.taskStatus == Task.Status.Available ||
+                t.taskStatus == Task.Status.DueSoon ||
+                t.taskStatus == Task.Status.Next ||
+                t.taskStatus == Task.Status.Overdue)
+          )
+        )
+        .forEach((task: Task) => {
+          if (task.containingProject && toFocus.indexOf(task.containingProject) === -1) {
+            toFocus.push(task.containingProject);
+          }
+        });
+
+      document.windows[0].perspective = Perspective.BuiltIn.Projects;
+      document.windows[0].focus = toFocus as SectionArray;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  });
+
+  return action;
+})();
