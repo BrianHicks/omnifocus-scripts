a84210d0aa602c357e3e73763bfde41e2ddb3f8f
Brian Hicks <brian@brianthicks.com>
2023-05-22 13:45:06 -0500

    add more globals

--- types/omnifocus-globals.d.ts
+++ types/omnifocus-globals.d.ts
@@ -8,6 +8,15 @@ declare const inbox: Inbox;
 
 declare const flattenedProjects: ProjectArray;
 
+declare const projects: ProjectArray;
+
 declare const flattenedTags: TagArray;
 
 declare const flattenedTasks: TaskArray;
+
+declare const library: Library;
+
+declare const moveSections = function (
+  sections: Array<Project | Folder>,
+  position: Folder | Folder.ChildInsertionLocation
+): void {};
